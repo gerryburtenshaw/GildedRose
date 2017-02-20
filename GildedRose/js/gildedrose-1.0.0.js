bddc9e8167abec483d533231783c1a228f9149a6
Gerry Burtenshaw <gerry@legitmix.com>
2017-02-20 15:31:32 -0500

    Created a javascript module for gildedrose functionality

---
+++ GildedRose/js/gildedrose-1.0.0.js
@@ -0,0 +1,25 @@
+﻿'use strict'
+
+window.GildedRose = window.GildedRose || (function () {
+   var _module = {};
+   var uriPrefix = 'api/';
+   var itemApi = uriPrefix + 'item';
+
+   _module.inventory = function (callback) {
+      $.getJSON(itemApi).done(callback);
+   }
+
+   _module.buyItem = function (data) {
+      $.ajax({
+         url: itemApi + '/' + data.itemname + '/buy',
+         headers: {
+            "Authorization": "Basic " + btoa(data.username + ":" + data.password)
+         },
+         success: data.success,
+         error: data.error
+      });
+   }
+
+   return _module;
+})();
+
